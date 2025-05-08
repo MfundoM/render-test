d2f4c09f0c84d2bef1dad5b4248ce9782b6cd55d
Mfundo Mthethwa <mfundo.nyambose@gmail.com>
2025-05-08 10:31:38 +0200

    docker file edit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/run-migrations', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations completed!';
 });
